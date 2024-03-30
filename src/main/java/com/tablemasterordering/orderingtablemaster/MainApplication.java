98ace534df7e26b33eec9c712895a42bc4481a25
Ayaan <94982193+incognito-root@users.noreply.github.com>
2024-03-30 08:22:44 +0500

    initial dashboard design started

--- src/main/java/com/tablemasterordering/orderingtablemaster/MainApplication.java
+++ src/main/java/com/tablemasterordering/orderingtablemaster/MainApplication.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-screen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
 
         stage.setScene(scene);
